dfb01201c521387e904d723db8b05a8723f2f5b8
Vadym Voiedilo <asd4s.profile@gmail.com>
2023-09-03 15:57:55 +0300

    Missing files were restored v2

---
+++ src/app/components/recipe-book/recipe.resolver.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import {
+  Router, Resolve,
+  RouterStateSnapshot,
+  ActivatedRouteSnapshot
+} from '@angular/router';
+import { Observable, of } from 'rxjs';
+import {RecipeService} from "../../services/recipe.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecipeResolver implements Resolve<boolean> {
+  constructor(
+    private recipeService: RecipeService,
+    private router: Router
+  ) {
+  }
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
+    const id: number = +route.params['id'];
+    const recipes = this.recipeService.getRecipes();
+
+    if (!recipes[id]) {
+      this.router.navigate(['recipes']);
+    }
+
+    return of(true);
+  }
+}
