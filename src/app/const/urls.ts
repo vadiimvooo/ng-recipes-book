c7d9df1a5bcb4148ad1464d561d7fd3abf66f3f9
Vadym Voiedilo <asd4s.profile@gmail.com>
2023-09-03 16:20:21 +0300

    Auto-generated commit [ci skip]

--- src/app/const/urls.ts
+++
@@ -1,9 +0,0 @@
-const apiKey = 'AIzaSyCOgDlm0H1UIsn3Vxl9cUQd1lZDF9B9No4'
-export const baseAuthUrl = `https://identitytoolkit.googleapis.com/v1/accounts`;
-const baseUrl = "https://course-app-backend-default-rtdb.europe-west1.firebasedatabase.app"
-
-export const urls = {
-  recipes: `${baseUrl}/recipes.json`,
-  signUp: `${baseAuthUrl}:signUp?key=${apiKey}`,
-  login: `${baseAuthUrl}:signInWithPassword?key=${apiKey}`
-}
